5837df6548267776e4034f5a4f43a04bb43350cd
sunil kumar reddy <sunilkumarreddy6109@gmail.com>
2026-02-23 23:17:34 +0530

    Return syntax program,Understanding syntax

---
+++ src/com/sunil/methods/Greeting.java
@@ -0,0 +1,11 @@
+package com.sunil.methods;
+import java.util.Scanner;
+public class Greeting {
+    public static void main(String[] args) {
+        greetings();
+    }
+    static void greetings() {
+        System.out.println("Functions and Methods");
+
+    }
+}
