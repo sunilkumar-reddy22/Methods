dad13b39409230176c01c1aa70d9f98163867937
sunil kumar reddy <sunilkumarreddy6109@gmail.com>
2026-02-24 23:22:24 +0530

    Added string method

---
+++ src/com/sunil/methods/Stringexample.java
@@ -0,0 +1,13 @@
+package com.sunil.methods;
+
+public class Stringexample {
+    public static void main(String[] args) {
+        System.out.println(greet());
+    }
+
+    static String greet() {
+        String greeting = ("hello, I just love java");
+        return greeting;
+    }
+}
+
