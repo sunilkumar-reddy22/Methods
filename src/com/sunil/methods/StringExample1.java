181e757731058932d09e47579375233fb1c2a210
sunil kumar reddy <sunilkumarreddy6109@gmail.com>
2026-02-24 23:40:06 +0530

    practicing methods with string type

---
+++ src/com/sunil/methods/StringExample1.java
@@ -0,0 +1,11 @@
+package com.sunil.methods;
+
+public class StringExample1 {
+    public static void main(String[] args) {
+        System.out.println ( rookie());
+    }
+    static String rookie() {
+    String lightning =( "hello,mcqueen");
+    return lightning;
+    }
+}
