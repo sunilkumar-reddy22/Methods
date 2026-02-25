4dc040efff18e92ad183002f0a57840694ed4bff
sunil kumar reddy <sunilkumarreddy6109@gmail.com>
2026-02-25 22:18:16 +0530

    passing parameters program

---
+++ src/com/sunil/methods/Parameters.java
@@ -0,0 +1,14 @@
+package com.sunil.methods;
+
+public class Parameters {
+    public static void main(String[] args) {
+       int ans =  num(20,30);
+        System.out.println(ans);
+
+    }
+    static int num(int a,int b) {
+    int sum = a + b;
+    return sum;
+    }
+
+}
