0db1c232b81baa31cdab83f0129bddbeb2f7d659
sunil kumar reddy <sunilkumarreddy6109@gmail.com>
2026-03-08 22:21:45 +0530

    basic method callih

---
+++ src/wordExamplle.java
@@ -0,0 +1,9 @@
+public class wordExamplle {
+    public static void main(String[] args) {
+        System.out.println(welcome());
+    }
+    static String welcome()  {
+    String a = "hello! how are you.";
+    return a;
+    }
+}
