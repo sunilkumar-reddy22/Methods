95a1474a657571608b384020dc931a421a169fe8
sunil kumar reddy <sunilkumarreddy6109@gmail.com>
2026-03-09 23:39:31 +0530

    basic arithmetic using arguments -> passing the parameters

---
+++ src/com/sunil/methods/Multiply.java
@@ -0,0 +1,12 @@
+package com.sunil.methods;
+
+public class Multiply {
+    public static void main(String[] args) {
+       int ans = mul(40,2);
+        System.out.println("Result = " + ans);
+    }
+    static int mul(int a,int b) {
+    int k = a * b;
+    return k;
+    }
+}
