52bd93494a9b60d531bd871e1f71e34925c74ee2
sunil kumar reddy <sunilkumarreddy6109@gmail.com>
2026-03-10 00:02:13 +0530

    String operation using parameeters with Scanner input

--- src/com/sunil/methods/StringExample2.java
+++ src/com/sunil/methods/StringExample2.java
@@ -1,8 +1,11 @@
 package com.sunil.methods;
-
+import java.util.Scanner;
 public class StringExample2 {
     public static void main(String[] args) {
-       String ans =  greet("lightning");
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter name: ");    ;
+        String name = in.next();
+       String ans =  greet(name);
         System.out.println(ans);
     }
     static String greet (String name) {
