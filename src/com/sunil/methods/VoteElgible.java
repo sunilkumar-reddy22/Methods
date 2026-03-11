096aa7fc6d7108307e7684c05475ce78cda06614
sunil kumar reddy <sunilkumarreddy6109@gmail.com>
2026-03-11 08:14:25 +0530

    Modified the code and used method calling inside the if condition

--- src/com/sunil/methods/VoteElgible.java
+++ src/com/sunil/methods/VoteElgible.java
@@ -1,22 +1,25 @@
 package com.sunil.methods;
-import java.sql.SQLOutput;
 import java.util.Scanner;
 public class VoteElgible {
     public static void main(String[] args) {
-       int age = vote();
 
-    }
-    static int vote() {
-    Scanner in = new Scanner(System.in);
+        Scanner in = new Scanner(System.in);
         System.out.println("Hello! enter your age: ");
         int age = in.nextInt();
-        if(age >= 18) {
+
+        if (iseligible(age)) {
             System.out.println("You're! Elgibile for voting");
-        }
-        else {
+        } else {
             System.out.println("Not elgible");
         }
-        return age;
+    }
 
+    static boolean iseligible(int age) {
+        return age >= 18;
     }
 }
+
+
+
+
+
