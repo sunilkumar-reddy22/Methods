909f1fac8ded5cc80a3c59b9dd27a98a4a6dd99c
sunil kumar reddy <sunilkumarreddy6109@gmail.com>
2026-03-16 22:11:30 +0530

    amstrong program

---
+++ src/com/sunil/methods/Amstrongnumber.java
@@ -0,0 +1,22 @@
+package com.sunil.methods;
+import java.util.Scanner;
+public class Amstrongnumber {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter number: ");
+        int num = in.nextInt();
+        System.out.println(isamstrong(num));
+    }
+    static boolean isamstrong(int a) {
+        int original = a;
+        int sum = 0;
+
+        while (a > 0) {
+            int rem = a % 10;
+            a = a / 10;
+            sum = sum + rem * rem * rem;
+        }
+        return sum == original;
+    }
+
+}
