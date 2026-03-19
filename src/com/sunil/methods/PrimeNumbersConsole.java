c48315af3f3cc60a9ece332af8cb63553b36b468
sunil kumar reddy <sunilkumarreddy6109@gmail.com>
2026-03-19 23:54:07 +0530

    Printing prime nummbers from user given start to end numbers -->using metod calling and returing value

---
+++ src/com/sunil/methods/PrimeNumbersConsole.java
@@ -0,0 +1,35 @@
+package com.sunil.methods;
+import java.util.Scanner;
+public class PrimeNumbersConsole {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter Start: ");
+        int a = in.nextInt();
+        System.out.println("Enter end: ");
+        int b = in.nextInt();
+        isprimeBetween(a, b);
+
+    }
+
+    static void isprimeBetween(int start, int end) {
+        for (int i = start; i <= end; i++) {
+            if (isprime(i)) {
+                System.out.println(i);
+            }
+
+        }
+    }
+
+    static boolean isprime(int num) {
+        if (num <= 1) {
+            return false;
+        }
+        for (int i = 2;  i * i <= num; i++) {
+            if (num % i  == 0) {
+                return false;
+            }
+
+        }
+        return true;
+    }
+}
